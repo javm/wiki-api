71442997163648b11cde316af2186cac1371b540
José Antonio Villarreal <quetzal1910@gmail.com>
2014-08-19 15:05:13 -0500

    First part

--- api.js
+++ api.js
@@ -1,12 +1,26 @@
 var express = require('express');
-var js = require('fs');
+var fs = require('fs');
 var bodyParser = require('body-parser');
-
 var app = express();
 app.use(bodyParser());
+var htmlPath = './files'
+var Plane = require('./models/plane');
+var plane = new Plane('myPlane');
 
 app.get('/', function(req, res, next){
   res.json({msg: 'Wiki-API'});
 });
+app.get('/news/:post', function(req, res, next){
+  var post = req.params.post;
+  var planeCrashPath = htmlPath+'/'+post+'.html'
+  //Printing fibonacci for each request
+  console.log(plane.fibonacci(34));
+  res.sendfile(planeCrashPath);
+});
+
+app.post('/news/:post', function(req, res, next){
+  
+})
+
 
 app.listen(3000);
--- api.test.js
+++ api.test.js
@@ -12,4 +12,17 @@ describe('wiki-api', function(){
 	done();
       });
   });
+
+  it('gets the plane crash news', function(done){
+    superagent.get('http://localhost:3000/news/Latest_plane_crash')
+      .send()
+      .end(function (err, res){
+	if(err)
+	  console.log(err);
+	console.log(res);
+	expect(res.text).to.be.a('string');
+	done();
+      });
+  });
+
 });
